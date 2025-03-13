e63baafb91a61e3785f320850aa0d44412308034
NotMyPersonalAccount <kevinzj710@gmail.com>
2025-03-12 23:26:45 -0700

    Implement basic multi-page navigation

--- src/main/java/s25/cs151/bookieprofessor/Main.java
+++ src/main/java/s25/cs151/bookieprofessor/Main.java
@@ -5,19 +5,29 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
-import java.io.IOException;
-
 public class Main extends Application {
+    private static Stage appStage;
+
     @Override
-    public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("main-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 1080, 720);
+    public void start(Stage stage) {
+        appStage = stage;
+
         stage.setTitle("BookieProfessor");
-        stage.setScene(scene);
+        switchPage("main-view.fxml");
         stage.show();
     }
 
     public static void main(String[] args) {
         launch();
+    }
+
+    public static void switchPage(String page) {
+        try {
+            FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource(page));
+            Scene scene = new Scene(fxmlLoader.load(), 1080, 720);
+            appStage.setScene(scene);
+        } catch (Exception e) {
+            // NOOP
+        }
     }
 }
