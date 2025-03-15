a4a97266423fd713d84c1822eeacb4fbe06bd531
Akash-School <158226059+Akash-School@users.noreply.github.com>
2025-03-15 01:31:51 -0700

    Added remaining fields to Define Semester Hours page (#2)

--- src/main/java/s25/cs151/application/DefineSemesterHoursController.java
+++ src/main/java/s25/cs151/application/DefineSemesterHoursController.java
@@ -22,7 +22,24 @@ public class DefineSemesterHoursController
         // Create the FormsFX form
         Form form = Form.of(
             Group.of(
-                    Field.ofSingleSelectionType(Arrays.asList("Spring", "Summer", "Fall", "Winter"), 1).label("Semester")
+                    Field.ofSingleSelectionType(Arrays.asList("Spring", "Summer", "Fall", "Winter"), 0).label("Semester"),
+                    Field.ofStringType("2025")
+                            .label("Year (4-digit)"),
+                    Field.ofBooleanType(false).label("Monday"),
+                    Field.ofBooleanType(false).label("Tuesday"),
+                    Field.ofBooleanType(false).label("Wednesday"),
+                    Field.ofBooleanType(false).label("Thursday"),
+                    Field.ofBooleanType(false).label("Friday"),
+                    Field.ofStringType("")
+                            .label("From Hour (HH:MM)"),
+                    Field.ofStringType("")
+                            .label("To Hour (HH:MM)"),
+                    Field.ofStringType("")
+                            .label("Course Code"),
+                    Field.ofStringType("")
+                            .label("Course Name"),
+                    Field.ofStringType("")
+                            .label("Section Number")
             )
         ).title("Define Semester Hours");
 
