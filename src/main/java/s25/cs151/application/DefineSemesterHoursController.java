60da3d274c82f64f5c5e3e2c83cf002ed73c07ee
NotMyPersonalAccount <kevinzj710@gmail.com>
2025-03-15 02:35:52 -0700

    wip! finish form elements

--- src/main/java/s25/cs151/application/DefineSemesterHoursController.java
+++ src/main/java/s25/cs151/application/DefineSemesterHoursController.java
@@ -3,10 +3,13 @@ package s25.cs151.application;
 import com.dlsc.formsfx.model.structure.Field;
 import com.dlsc.formsfx.model.structure.Form;
 import com.dlsc.formsfx.model.structure.Group;
+import com.dlsc.formsfx.model.validators.IntegerRangeValidator;
+import com.dlsc.formsfx.view.controls.SimpleCheckBoxControl;
 import com.dlsc.formsfx.view.renderer.FormRenderer;
 import javafx.fxml.FXML;
 import javafx.scene.layout.VBox;
 
+import java.time.Year;
 import java.util.Arrays;
 
 public class DefineSemesterHoursController
@@ -23,23 +26,13 @@ public class DefineSemesterHoursController
         Form form = Form.of(
             Group.of(
                     Field.ofSingleSelectionType(Arrays.asList("Spring", "Summer", "Fall", "Winter"), 0).label("Semester"),
-                    Field.ofStringType("2025")
-                            .label("Year (4-digit)"),
-                    Field.ofBooleanType(false).label("Monday"),
-                    Field.ofBooleanType(false).label("Tuesday"),
-                    Field.ofBooleanType(false).label("Wednesday"),
-                    Field.ofBooleanType(false).label("Thursday"),
-                    Field.ofBooleanType(false).label("Friday"),
-                    Field.ofStringType("")
-                            .label("From Hour (HH:MM)"),
-                    Field.ofStringType("")
-                            .label("To Hour (HH:MM)"),
-                    Field.ofStringType("")
-                            .label("Course Code"),
-                    Field.ofStringType("")
-                            .label("Course Name"),
-                    Field.ofStringType("")
-                            .label("Section Number")
+                    Field.ofIntegerType(Year.now().getValue())
+                            .validate( IntegerRangeValidator.between(1000, 9999, "Four digits expected"))
+                            .label("Year"),
+                    Field.ofMultiSelectionType(Arrays.asList("Monday", "Tuesday", "Wednesday", "Thursday", "Friday"))
+                            .label("Days")
+                            .required("At least one day must be selected")
+                            .render(new SimpleCheckBoxControl<>())
             )
         ).title("Define Semester Hours");
 
