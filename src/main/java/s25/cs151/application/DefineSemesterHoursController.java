203cae82e83beae239feef0923a46ed496e52e5d
NotMyPersonalAccount <kevinzj710@gmail.com>
2025-03-22 06:13:42 -0700

    Fix the year input not updating the model

--- src/main/java/s25/cs151/application/DefineSemesterHoursController.java
+++ src/main/java/s25/cs151/application/DefineSemesterHoursController.java
@@ -3,6 +3,7 @@ package s25.cs151.application;
 import com.dlsc.formsfx.model.structure.Field;
 import com.dlsc.formsfx.model.structure.Form;
 import com.dlsc.formsfx.model.structure.Group;
+import com.dlsc.formsfx.model.structure.IntegerField;
 import com.dlsc.formsfx.model.validators.IntegerRangeValidator;
 import com.dlsc.formsfx.view.controls.SimpleCheckBoxControl;
 import com.dlsc.formsfx.view.renderer.FormRenderer;
@@ -12,6 +13,7 @@ import javafx.scene.layout.VBox;
 import s25.cs151.application.model.SemesterHours;
 
 import java.sql.SQLException;
+import java.time.Year;
 import java.util.ArrayList;
 
 public class DefineSemesterHoursController
@@ -32,14 +34,17 @@ public class DefineSemesterHoursController
         // Initialize model for this form
         this.semesterHours = new SemesterHours();
 
+        // `Field.ofIntegerType` does not appear to correctly bind its value property to the passed property.
+        IntegerField yearField = Field.ofIntegerType(Year.now().getValue())
+                .validate( IntegerRangeValidator.between(1000, 9999, "Four digits expected"))
+                .label("Year");
+        this.semesterHours.year.bind(yearField.valueProperty());
         // Create the FormsFX form
         this.form = Form.of(
             Group.of(
                     Field.ofSingleSelectionType(this.semesterHours.allSeasons, this.semesterHours.semester)
                             .label("Semester"),
-                    Field.ofIntegerType(this.semesterHours.year)
-                            .validate( IntegerRangeValidator.between(1000, 9999, "Four digits expected"))
-                            .label("Year"),
+                    yearField,
                     Field.ofMultiSelectionType(this.semesterHours.allDays, this.semesterHours.days)
                             .label("Days")
                             .required("At least one day must be selected")
