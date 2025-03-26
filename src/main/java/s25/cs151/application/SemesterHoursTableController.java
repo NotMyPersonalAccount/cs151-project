35a2f39ad0717b3ed0dc822555c60feb632ed257
NotMyPersonalAccount <kevinzj710@gmail.com>
2025-03-26 11:05:03 -0700

    Cleanup SemesterHoursTableController

--- src/main/java/s25/cs151/application/SemesterHoursTableController.java
+++ src/main/java/s25/cs151/application/SemesterHoursTableController.java
@@ -1,7 +1,7 @@
 package s25.cs151.application;
 
-import javafx.beans.property.ObjectProperty;
-import javafx.beans.property.SimpleObjectProperty;
+import javafx.beans.binding.Bindings;
+import javafx.beans.property.ListProperty;
 import javafx.collections.FXCollections;
 import javafx.fxml.FXML;
 import javafx.scene.control.TableColumn;
@@ -9,6 +9,7 @@ import javafx.scene.control.TableView;
 import javafx.scene.control.cell.PropertyValueFactory;
 import s25.cs151.application.model.SemesterHours;
 
+import java.util.Comparator;
 import java.util.Map;
 
 public class SemesterHoursTableController {
@@ -22,6 +23,8 @@ public class SemesterHoursTableController {
     @FXML
     protected TableColumn<SemesterHours, String> days;
 
+    protected static final Map<String, Integer> SEMESTER_ORDERING = Map.of("Spring", 0, "Summer", 1, "Fall", 2, "Winter", 3);
+
     @FXML
     protected void initialize() {
         // Connect table columns with the model.
@@ -30,20 +33,12 @@ public class SemesterHoursTableController {
         // Days in the model are a List<String>, but we want to display a comma-separated string. Create an observable
         // string with commas from the observable list.
         this.days.setCellValueFactory(p -> {
-            SemesterHours semesterHours = p.getValue();
-            ObjectProperty<String> days = new SimpleObjectProperty<>(String.join(", ", semesterHours.days.getValue()));
-            // Overkill for this project, but keep the string in sync with the list.
-            semesterHours.days.addListener((_, _, newValue) -> {
-                days.setValue(String.join(", ", newValue));
-            });
-            return days;
+            ListProperty<String> daysProperty = p.getValue().days;
+            return Bindings.createStringBinding(() -> String.join(", ", daysProperty.getValue()), daysProperty);
         });
 
-        // Set the sorting order of the semester column
-        this.semester.setComparator((a, b) -> {
-            Map<String, Integer> order = Map.of("Spring", 0, "Summer", 1, "Fall", 2, "Winter", 3);
-            return order.get(a).compareTo(order.get(b));
-        });
+        // Set the sorting order of the semester column.
+        this.semester.setComparator(Comparator.comparing(SEMESTER_ORDERING::get));
 
         // Set items in the table.
         this.table.setItems(FXCollections.observableList(DatabaseHelper.getAllSemesterHours()));
