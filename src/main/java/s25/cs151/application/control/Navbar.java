90f9bd3bb8a777a3a809ee12d2e4dc113de5efa3
NotMyPersonalAccount <kevinzj710@gmail.com>
2025-04-08 16:07:56 -0700

    Override focus on navigation

--- src/main/java/s25/cs151/application/control/Navbar.java
+++ src/main/java/s25/cs151/application/control/Navbar.java
@@ -9,6 +9,8 @@ import s25.cs151.application.Main;
 import java.io.IOException;
 
 public class Navbar extends FlowPane {
+    protected static String activePage = "main-view.fxml";
+
     @FXML
     protected Button home;
     @FXML
@@ -32,10 +34,32 @@ public class Navbar extends FlowPane {
         }
 
         // Handle button actions
-        this.home.setOnAction(_ -> Main.switchPage("main-view.fxml"));
-        //this.schedules.setOnAction(_ -> Main.switchPage("schedules.fxml"));
-        this.semester_hours.setOnAction(_ -> Main.switchPage("semester-hours.fxml"));
-        this.courses.setOnAction(_ -> Main.switchPage("courses.fxml"));
-        this.time_slots.setOnAction(_ -> Main.switchPage("semester-time-slots.fxml"));
+        this.home.setOnAction(_ -> this.switchPage("main-view.fxml"));
+        //this.schedules.setOnAction(_ -> this.switchPage("schedules.fxml"));
+        this.semester_hours.setOnAction(_ -> this.switchPage("semester-hours.fxml"));
+        this.courses.setOnAction(_ -> this.switchPage("courses.fxml"));
+        this.time_slots.setOnAction(_ -> this.switchPage("semester-time-slots.fxml"));
+    }
+
+    @FXML
+    public void initialize() {
+        // A hack to keep focus on the clicked button when recreating the scene on page switches.
+        this.sceneProperty().addListener((_, _, _) -> {
+            Button activeButton = switch (activePage) {
+                case "semester-hours.fxml" -> this.semester_hours;
+                case "courses.fxml" -> this.courses;
+                case "semester-time-slots.fxml" -> this.time_slots;
+                default -> this.home;
+            };
+
+            activeButton.requestFocus();
+        });
+    }
+
+    protected void switchPage(String page) {
+        if (activePage.equals(page)) return;
+
+        activePage = page;
+        Main.switchPage(page);
     }
 }
