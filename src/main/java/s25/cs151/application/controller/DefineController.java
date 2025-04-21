bd645266a561b25bbf82615c670333415fd3f1b6
NotMyPersonalAccount <kevinzj710@gmail.com>
2025-04-21 00:46:41 -0700

    Implement logic for beginning data model edit

--- src/main/java/s25/cs151/application/controller/DefineController.java
+++ src/main/java/s25/cs151/application/controller/DefineController.java
@@ -5,7 +5,9 @@ import javafx.fxml.FXML;
 import s25.cs151.application.Main;
 import s25.cs151.application.control.DefinePage;
 import s25.cs151.application.forms.IFormCreator;
+import s25.cs151.application.model.IModel;
 import s25.cs151.application.utils.IDatabaseExecutor;
+import s25.cs151.application.utils.IDatabaseUpdater;
 
 import java.sql.SQLException;
 
@@ -14,33 +16,56 @@ import java.sql.SQLException;
  * forms.
  * @param <T> the model type being defined in the Define page
  */
-abstract public class DefineController<T> {
+abstract public class DefineController<T extends IModel<T>> {
     @FXML
     protected DefinePage page;
 
     protected Form form;
     protected IFormCreator<T> formCreator;
 
+    protected T existingModel;
     protected T currentModel;
 
     protected IDatabaseExecutor<T> databaseInserter;
+    protected IDatabaseUpdater<T> databaseUpdater;
 
+    // TODO: Remove this constructor once all data models can be edited
     public DefineController(T currentModel, IFormCreator<T> formCreator, IDatabaseExecutor<T> databaseInserter) {
         this.currentModel = currentModel;
         this.formCreator = formCreator;
         this.databaseInserter = databaseInserter;
     }
 
+    public DefineController(T currentModel, IFormCreator<T> formCreator, IDatabaseExecutor<T> databaseInserter, IDatabaseUpdater<T> databaseUpdater) {
+        this(currentModel, formCreator, databaseInserter);
+        this.databaseUpdater = databaseUpdater;
+    }
+
     @FXML
     public void initialize() {
-        this.form = this.formCreator.create(this.currentModel);
-        this.page.setForm(this.form);
+        this.initializeForm();
         this.page.setOnSubmit(this::onSubmit);
     }
 
-    public void onSubmit() {
+    protected void initializeForm() {
+        this.form = this.formCreator.create(this.currentModel);
+        this.page.setForm(this.form);
+    }
+
+    public void startEditing(T existingModel) {
+        this.existingModel = existingModel;
+        this.currentModel = existingModel.copy();
+        this.initializeForm();
+        this.page.setEditing(true);
+    }
+
+    protected void onSubmit() {
         try {
-            this.databaseInserter.execute(this.currentModel);
+            if (this.existingModel == null) {
+                this.databaseInserter.execute(this.currentModel);
+            } else {
+                this.databaseUpdater.update(this.existingModel, this.currentModel);
+            }
             Main.switchPage(this.page.getParentPage());
         } catch (SQLException e) {
             // SQLite error code 19 is constraint failed (in our case, because of primary key uniqueness).
